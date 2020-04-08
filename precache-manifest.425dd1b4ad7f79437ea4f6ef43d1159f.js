e79d81dcab71f4cd17a46b0aaa8a5ed1bc25f017
20minhajj <harith20@icloud.com>
2020-04-08 12:39:58 +0300

    Updates

---
+++ precache-manifest.425dd1b4ad7f79437ea4f6ef43d1159f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac39b7ec992ea8d164b1f19f88e929bf",
+    "url": "/minhajj/index.html"
+  },
+  {
+    "revision": "6c8a4df1f57416573696",
+    "url": "/minhajj/static/css/main.d0879fce.chunk.css"
+  },
+  {
+    "revision": "f362f1123142296b0b31",
+    "url": "/minhajj/static/js/2.6b0b3df5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/minhajj/static/js/2.6b0b3df5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c8a4df1f57416573696",
+    "url": "/minhajj/static/js/main.5c583cf5.chunk.js"
+  },
+  {
+    "revision": "4649dbf7af98a1948142",
+    "url": "/minhajj/static/js/runtime-main.5fa3ab1f.js"
+  },
+  {
+    "revision": "60626c8a34252730eb63c9dc2aea6d03",
+    "url": "/minhajj/static/media/andela.60626c8a.jpeg"
+  },
+  {
+    "revision": "18608faf268b6c67b713d998764ced84",
+    "url": "/minhajj/static/media/download.18608faf.png"
+  },
+  {
+    "revision": "dfe812c3fa5f8c9db8a3f01e9f072ed8",
+    "url": "/minhajj/static/media/dp.dfe812c3.jpg"
+  }
+]);
